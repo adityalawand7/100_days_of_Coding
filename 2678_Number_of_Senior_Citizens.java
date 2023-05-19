bee381e8f83cf56b6a9e5ca64f9a392765ce76c2
Aditya Lawand <79708044+adityalawand7@users.noreply.github.com>
2023-05-19 23:56:53 +0530

    Create 2678_Number_of_Senior_Citizens.java

---
+++ 2678_Number_of_Senior_Citizens.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int n = details.length, a = 0, count = 0;
+        for(int i = 0; i < n; i++){
+            String s = details[i].substring(11, 13);
+            a = Integer.parseInt(s);
+            if(a > 60)
+                count++;
+        }
+        return count;
+    }
+}
